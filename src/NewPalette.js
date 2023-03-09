b8267813492421d6ded2757f66139a2447d99eab
SinanSenkul <sinansenkul01@gmail.com>
2023-03-09 05:00:24 +0300

    continued developing new palette form

--- src/NewPalette.js
+++ src/NewPalette.js
@@ -1,18 +1,156 @@
 import React, { Component, useState } from "react";
 import { withStyles } from '@material-ui/styles';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { Button } from "@mui/material";
+import { ChromePicker } from "react-color";
+import { textColor } from "./textColor";
+
+const styles = {
+}
+
+const drawerWidth = 400;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: `-${drawerWidth}px`,
+        ...(open && {
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginLeft: 0,
+        }),
+    }),
+);
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: `${drawerWidth}px`,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
 
 function NewPalette(props) {
     const { classes } = props;
-    const styles = {
-        main: {
-        },
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+    var [colorPicked, setColor] = React.useState('blue');
+    var [textClr, setTextColor] = React.useState('white');
+    var [colors, setColors] = React.useState([]);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    function handleColorChange(color) {
+        setColor(colorPicked = color.hex);
+        let newTextColor = textColor(color.hex);
+        setTextColor(textClr = newTextColor);
+        console.log(colorPicked);
+    }
+
+    function addColor(){
+        setColors([...colors, colorPicked]);
     }
 
     return (
-        <div >
-            <h1>new palette</h1>
-        </div>
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Persistent drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="persistent"
+                anchor="left"
+                open={open}
+            >
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <Typography variant="h4">create a new palette</Typography>
+                <div className={classes.drawerButtonContainer}>
+                    <Button variant="contained" color="secondary">clear palette</Button>
+                    <Button variant="contained" color="primary">random color</Button>
+                </div>
+                <ChromePicker color={colorPicked} onChange={handleColorChange} />
+                <Button 
+                    style={{ backgroundColor: colorPicked, color: textClr }} 
+                    variant="contained" 
+                    color="primary"
+                    onClick={addColor}
+                >
+                    add color
+                </Button>
+            </Drawer>
+            <Main open={open}>
+                <DrawerHeader />
+                {colors.map(color => <h1>{color}</h1>)}
+            </Main>
+        </Box>
     );
 }
 
-export default NewPalette;
+export default withStyles(styles, { withTheme: true })(NewPalette);
